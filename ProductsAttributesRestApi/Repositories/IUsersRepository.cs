d8e8ed1d29edd642e3aadcb36db7c7e2f9ce7d71
TatjanaTomic <tatjanatomic997@yahoo.com>
2023-06-01 00:22:57 +0200

    Add users repository

---
+++ ProductsAttributesRestApi/Repositories/IUsersRepository.cs
@@ -0,0 +1,9 @@
+﻿using ProductsAttributesRestApi.Models.Entities;
+
+namespace ProductsAttributesRestApi.Repositories;
+
+public interface IUsersRepository
+{
+    Task<User?> GetUserByEmail(string email);
+    Task<List<User>> AddUser(User user);
+}
---
+++ ProductsAttributesRestApi/Repositories/Impl/UsersRepository.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductsAttributesAPI.Data;
+using ProductsAttributesRestApi.Models.Entities;
+
+namespace ProductsAttributesRestApi.Repositories.Impl
+{
+    public class UsersRepository : BaseRepository, IUsersRepository
+    {
+
+        public UsersRepository(DataContext dataContext) : base(dataContext)
+        {
+        }
+
+        public async Task<List<User>> AddUser(User user)
+        {
+            _dataContext.Users.Add(user);
+            _dataContext.SaveChanges();
+            return await _dataContext.Users.ToListAsync();        }
+
+        public async Task<User?> GetUserByEmail(string email)
+        {
+            return await _dataContext.Users.SingleOrDefaultAsync(u => u.Email == email);
+        }
+    }
+}
