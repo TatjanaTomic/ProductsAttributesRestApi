7c26b21b739c2af9300a27a72bc59cf3f5b92331
TatjanaTomic <tatjanatomic997@yahoo.com>
2023-06-02 12:48:16 +0200

    Remove unused class

--- ProductsAttributesRestApi/Exceptions/DuplicateEntryException.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ProductsAttributesRestApi.Exceptions;
-
-public class DuplicateEntryException : Exception
-{
-    public DuplicateEntryException(string message) : base(message) 
-    {
-
-    }
-}
